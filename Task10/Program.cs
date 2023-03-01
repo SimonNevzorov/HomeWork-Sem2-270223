e20bd8e4bd33a8fecd5b65f0e09f5bffc7a70838
Simon Nevzorov <sem.nevzorov@gmail.com>
2023-03-01 22:42:01 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,7 @@
+﻿Random rand = new Random();
+int number = rand.Next(100, 1000);
+
+Console.WriteLine("Случайное число = " + number);
+// numberA = number / 
+Console.WriteLine("Его вторая цифра = " + ((number / 10) % 10) );
+
